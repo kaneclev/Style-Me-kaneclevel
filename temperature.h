9acbf6f3226724561e2010a637a18b8e37319ad9
Kane Cleveland <kmclev21@stlawu.edu>
2023-09-06 11:25:58 -0400

    Added commetns to header file for clarity

--- temperature.h
+++ temperature.h
@@ -5,10 +5,10 @@
 #ifndef MGM1_STYLE_ME_KANECLEV_TEMPERATURE_H
 #define MGM1_STYLE_ME_KANECLEV_TEMPERATURE_H
 #include "main.cpp"
-
+// Define class in the header file.
 class temperature {
-    double temp;
-public:
+    double temp; // Private double temp.
+public: // Public methods; getters and setters. Changed the names of the methods for clarity.
     temperature();
     static double getFahrenheit();
     void setFahrenheit(double degrees);
